577ae25d530913da425ee4a573ffac699ecb45ce
Edwin <edwinombati99@gmail.com>
2020-05-01 09:19:04 -0800

    homegraph

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -194,6 +194,43 @@
                 </div>
             </div>
 
+            <!DOCTYPE html>
+            <html lang="en-US">
+            <body>
+
+            <h1>Graphical representation of members giving</h1>
+
+            <div id="piechart"></div>
+
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+            <script type="text/javascript">
+                // Load google charts
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
+
+                // Draw the chart and set the chart values
+                function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                        ['Giving', 'Amount'],
+                        ['Tithe', 200],
+                        ['Offering', 800],
+                        ['Camp meeting Offering', 600],
+                        ['Development', 2500],
+                        ['Benevolent Kitty', 850]
+                    ]);
+
+                    // Optional; add a title and set the width and height of the chart
+                    var options = {'title':'Memebers Giving ', 'width':550, 'height':400};
+
+                    // Display the chart inside the <div> element with id="piechart"
+                    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                    chart.draw(data, options);
+                }
+            </script>
+
+            </body>
+            </html>
 
 
 
