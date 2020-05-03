e12d46380068537a3c320d4ed2edb52dec605164
zalo <austinezaloa@gmail.com>
2020-05-03 23:33:35 +0300

    database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$DATABASE_URL=parse_url( 'postgres://qqkvknbnyhwusy:bd1e71ae21552b52e6ae35afbb2ba7d1e4a23a75ada6428d2e57440e69d535d2@ec2-54-165-36-134.compute-1.amazonaws.com:5432/ddf1dso810');
+$DATABASE_URL=parse_url( 'postgres://qqkvknbnyhwusy:bd1e71ae21552b52e6ae35afbb2ba7d1e4a23a75ada6428d2e57440e69d535d2@ec2-54-165-36-134.compute-1.amazonaws.com:5432/ddf1dso810vlnd');
 use Illuminate\Support\Str;
 
 return [
