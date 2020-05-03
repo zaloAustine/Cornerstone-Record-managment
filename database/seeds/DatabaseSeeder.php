19cf54d2bfe7bef641867c6b579aad62b897901a
zalo <austinezaloa@gmail.com>
2020-05-04 00:54:53 +0300

    heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
 
        // $this->call(User::class);
         factory(User::class)->create([
