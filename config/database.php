32b2345977a177ef4dace0c57d29b684adda970c
zalo <austinezaloa@gmail.com>
2020-05-03 23:31:36 +0300

    database

--- config/database.php
+++ config/database.php
@@ -65,7 +65,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => $DATABASE_URL,
             'host' => $DATABASE_URL['host'],
             'port' => $DATABASE_URL['port'],
             'database' => ltrim($DATABASE_URL['path'],"/"),
